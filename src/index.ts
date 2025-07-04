f63f1127dd0648cefc9a9c176ea61769d1849ba7
Jureg <filipe.barrosti@gmail.com>
2025-07-04 16:40:41 -0300

    ✨ add re exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import implementation from './fastify.implementation';
+export { default as fastify } from 'fastify';
+export { default as fastifyCookie } from '@fastify/cookie';
+export { default as fastifyHelmet } from '@fastify/helmet';
+export { default as fastifyMiddie } from '@fastify/middie';
+
+export { default as implementation } from './fastify.implementation';
 
 export type { ImplementationConfig } from './@types/implementation.config';
-
-export { implementation };
