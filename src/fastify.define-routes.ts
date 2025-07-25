62a042eafa00be4245ea19bd711aa8a50c7f5c60
Jureg <filipe.barrosti@gmail.com>
2025-07-25 18:20:16 -0300

    🔖 release 1.0.1

--- src/fastify.define-routes.ts
+++ src/fastify.define-routes.ts
@@ -3,46 +3,52 @@ import { readdirSync, statSync, existsSync } from 'fs';
 import { join, parse, resolve } from 'path';
 
 function getAllFilesPaths(dirPath: string, arrayOfFiles?: string[]): string[] {
-  if (!existsSync(dirPath)) {
-    return [];
-  }
+	if (!existsSync(dirPath)) {
+		return [];
+	}
 
-  const files = readdirSync(dirPath);
+	const files = readdirSync(dirPath);
 
-  const newArrayOfFiles = arrayOfFiles || [];
+	const newArrayOfFiles = arrayOfFiles || [];
 
-  files.forEach((fileName) => {
-    const localDir = join(dirPath, fileName);
+	files.forEach((fileName) => {
+		const localDir = join(dirPath, fileName);
 
-    if (statSync(localDir).isDirectory()) {
-      const directoryFiles = getAllFilesPaths(localDir, arrayOfFiles);
-      newArrayOfFiles.push(...directoryFiles);
-      return;
-    }
+		if (statSync(localDir).isDirectory()) {
+			const directoryFiles = getAllFilesPaths(localDir, arrayOfFiles);
+			newArrayOfFiles.push(...directoryFiles);
+			return;
+		}
 
-    newArrayOfFiles.push(localDir);
-  });
+		newArrayOfFiles.push(localDir);
+	});
 
-  return newArrayOfFiles;
+	return newArrayOfFiles;
 }
 
-export default async function defineRoutes(app: FastifyInstance, pathToControllerAsArray: string[][]): Promise<void> {
-  const basePath = pathToControllerAsArray.map((p) => join(...p));
+export default async function defineRoutes(
+	app: FastifyInstance,
+	pathToControllerAsArray: string[][]
+): Promise<void> {
+	const basePath = pathToControllerAsArray.map((p) => join(...p));
 
-  const promises = basePath
-    .map((p) =>
-      getAllFilesPaths(p)
-        .filter((filePath) => {
-          const fileName = parse(filePath).base;
-          return filePath.indexOf('.') !== 0 && (fileName === 'routes.ts' || fileName === 'routes.js');
-        })
-        .map(async (filePath) => {
-          const modulePath = resolve(p, filePath);
-          const routeModule = (await import(modulePath)).default;
-          app.register(routeModule.router, { prefix: routeModule.basePath });
-        }),
-    )
-    .flat();
+	const promises = basePath
+		.map((p) =>
+			getAllFilesPaths(p)
+				.filter((filePath) => {
+					const fileName = parse(filePath).base;
+					return (
+						filePath.indexOf('.') !== 0 &&
+						(fileName === 'routes.ts' || fileName === 'routes.js')
+					);
+				})
+				.map(async (filePath) => {
+					const modulePath = resolve(p, filePath);
+					const routeModule = (await import(modulePath)).default;
+					app.register(routeModule.router, { prefix: routeModule.basePath });
+				})
+		)
+		.flat();
 
-  await Promise.all(promises);
+	await Promise.all(promises);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,71 @@
-export { default as fastify } from 'fastify';
-export { default as fastifyCookie } from '@fastify/cookie';
-export { default as fastifyHelmet } from '@fastify/helmet';
-export { default as fastifyMiddie } from '@fastify/middie';
+import type { FastifyInstance } from 'fastify';
 
+export type { FastifyInstance };
+export { default as fastify } from 'fastify';
+
+import {
+	CookieSerializeOptions,
+	FastifyCookie,
+	FastifyCookieOptions,
+	ParseOptions,
+	SerializeOptions,
+	Sign,
+	Signer,
+	SignerFactory,
+	Unsign,
+	UnsignResult,
+	fastifyCookie,
+	sign,
+	signerFactory,
+	unsign,
+} from '@fastify/cookie';
+
+export type {
+	CookieSerializeOptions,
+	FastifyCookie,
+	FastifyCookieOptions,
+	ParseOptions,
+	SerializeOptions,
+	Sign,
+	Signer,
+	SignerFactory,
+	Unsign,
+	UnsignResult,
+};
+
+export { fastifyCookie, sign, signerFactory, unsign };
+
+import {
+	FastifyHelmetOptions,
+	FastifyHelmetRouteOptions,
+	fastifyHelmet,
+} from '@fastify/helmet';
+
+export type { FastifyHelmetOptions, FastifyHelmetRouteOptions };
+export { fastifyHelmet };
+
+import {
+	FastifyMiddieOptions,
+	Handler,
+	IncomingMessageExtended,
+	NextFunction,
+	NextHandleFunction,
+	SimpleHandleFunction,
+	fastifyMiddie,
+} from '@fastify/middie';
+
+export type {
+	FastifyMiddieOptions,
+	Handler,
+	IncomingMessageExtended,
+	NextFunction,
+	NextHandleFunction,
+	SimpleHandleFunction,
+};
+export { fastifyMiddie };
+
+export { default as defineRoutes } from './fastify.define-routes';
+export { default as commonRoutes } from './fastify.common-routes';
 export { default as implementation } from './fastify.implementation';
 
-export type { ImplementationConfig } from './@types/implementation.config';
+export * from './@types/implementation.config';
